b764c40e7b495a5022e06c9effde38e555e53ce1
Rajesh Jain <rajeshcjain81@gmail.com>
2017-03-31 19:17:28 +0530

    basic go commit

---
+++ 02_NumberSystem/02_binary/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("decimal : %d binary : %b hexidecimal : %#x  octal : %o character : %c \n", 42, 42, 42, 42, 42)
+}
---
+++ 02_NumberSystem/03_loop/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 100; i++ {
+		fmt.Printf("decimal %d, hexadecimal %#X binary %b UTF 8 %q \n", i, i, i, i)
+	}
+}
