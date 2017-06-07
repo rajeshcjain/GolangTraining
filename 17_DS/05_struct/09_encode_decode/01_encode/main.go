ce02e44a2882b50e6c187240e9aafb099d1a8a46
Rajesh Jain <rajeshcjain81@gmail.com>
2017-06-07 20:19:48 +0530

    commiting struct and encoding..decoding code

---
+++ 17_DS/05_struct/09_encode_decode/01_encode/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Person struct{
+	First string
+	Last string
+	Age int
+}
+
+/*
+encode means we are changing the encode it means change in to which humans can not understand and
+then send it over stream...For it we need writer to write.
+*/
+
+func main(){
+
+	p1 := Person{"James","Bond",32}
+
+	json.NewEncoder(os.Stdout).Encode(p1)
+}
---
+++ 17_DS/05_struct/09_encode_decode/02_decode/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"strings"
+	"fmt"
+)
+
+type Person struct{
+	First string
+	Last string
+	Age int
+}
+
+func main(){
+
+	bs := []byte(`{"First":"James","Last":"Bond","Age":23}`)
+
+	//This is a simulation of json coming from somewhere as a stream..So we need to crearte
+	//the reader.
+	reader := strings.NewReader(string(bs))
+
+	var p1 Person
+	err := json.NewDecoder(reader).Decode(&p1)
+
+	if err != nil{
+		panic(err)
+	}
+
+	fmt.Println(p1)
+
+
+}
